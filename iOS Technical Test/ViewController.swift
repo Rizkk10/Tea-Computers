e6e6185612053f4ff93157769a33d92f1030c8e5
Rezk <rezk@MacBook-Pro.local>
2023-06-18 00:28:34 +0200

    create the project

---
+++ iOS Technical Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Technical Test
+//
+//  Created by Rezk on 18/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
